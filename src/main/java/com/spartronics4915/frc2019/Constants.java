d5a827f7a8f66a2253ed5b1a38dc45ce3c5a58cc
NJ <rose@Noah.local>
2019-02-01 17:24:23 -0800

    Constants fixes -- removed shoot motors, turret stuff from a long time ago

--- src/main/java/com/spartronics4915/frc2019/Constants.java
+++ src/main/java/com/spartronics4915/frc2019/Constants.java
@@ -111,12 +111,6 @@ public class Constants
     public static final int kRightDriveSlaveAId = 2;
     public static final double kDriveEncoderPPR = 1440.0; // PPR (1440) = CPR (360) * 4 (because quadrature)
 
-    // Turret
-    public static final int kTurretMotorId = 5; // TODO: Figure out the correct motor ID
-    // We're using the CTRE Mag encoders: https://content.vexrobotics.com/vexpro/pdf/Magnetic-Encoder-User's-Guide-01282016.pdf
-    public static final double kTurretPPR = 4096.0;
-    public static final double kTurretUnitsPerRev = 13653; // 4096 Quadrature CPR * (10 / 3) Belt reduction
-
     // Control Board
     public static final int kDriveJoystickPort = 0;
     public static final int kMainButtonBoardPort = 1;
@@ -129,8 +123,6 @@ public class Constants
     public static final int kClimberSolenoidId = 2;
 
     // Cargo Ramp
-    //public static final int kShootMotorLeftId = 8;
-    //public static final int kShootMotorRightId = 9;
     public static final int kRampMotorId = 5;
     public static final int kFlipperSolenoidId = 3; // A3 FIXME:!!!
     public static final int kShootSpeed = 1; // tune
