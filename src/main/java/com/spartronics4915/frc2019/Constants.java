1073b48d5f4129402edf89dece49ea736d433fe7
Austin Smith <domoking56@gmail.com>
2019-02-15 15:45:21 -0800

    Woops, wrong number in the IDs in the last commit, fixed it now.

--- src/main/java/com/spartronics4915/frc2019/Constants.java
+++ src/main/java/com/spartronics4915/frc2019/Constants.java
@@ -155,7 +155,7 @@ public class Constants
     public static final int kRearLeftSolenoid2 = 5; // Retract
     public static final int kRearRightSolenoidId1 = 6; // Extend
     public static final int kRearRightSolenoidId2 = 7; // Retract
-    public static final int kDownwardFrontIRSensorID = 0;
-    public static final int kDownwardRearIRSensorID = 1;
+    public static final int kDownwardFrontIRSensorID = 2;
+    public static final int kDownwardRearIRSensorID = 3;
     public static final double kIRSensorTriggerDistance = 2.5;
 }
