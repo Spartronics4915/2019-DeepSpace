4ec17b3a379feadbd7c7baeaa59a1b5df25bb1b3
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2019-02-17 13:21:34 -0800

    Fix some units in constants and tests

--- src/test/java/com/spartronics4915/frc2019/TestSystemTest.java
+++ src/test/java/com/spartronics4915/frc2019/TestSystemTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.spartronics4915.lib.geometry.Pose2d;
+import com.spartronics4915.lib.geometry.Rotation2d;
+
 public class TestSystemTest
 {
 
