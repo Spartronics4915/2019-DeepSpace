a3cd8d2152b8e9b192972ddab5096c06184e4322
j-james <j-james@users.noreply.github.com>
2019-02-17 16:34:56 -0800

    Changes made during intake testing

--- src/main/java/com/spartronics4915/frc2019/subsystems/CargoIntake.java
+++ src/main/java/com/spartronics4915/frc2019/subsystems/CargoIntake.java
@@ -43,13 +43,13 @@ public class CargoIntake extends Subsystem
 
     private static final boolean kSolenoidExtend = true;
     private static final boolean kSolenoidRetract = false;
-    private static final double kIntakeSpeed = -0.5;
+    private static final double kIntakeSpeed = -1.0;
     private static final double kEjectSpeed = 0.5;
     private static final double kIntakeClimbSpeed = -0.2;
 
     private Solenoid mSolenoid = null;
     private Solenoid mSolenoidClimb = null;
-    private TalonSRX mMotorRight = null;
+    private TalonSRX mMotorRight = null; // right and left are switched?
     private TalonSRX mMotorLeft = null;
     private IRSensor mSensor = null;
 
