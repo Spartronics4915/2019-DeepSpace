a84fdcc2d623611699b0bdff7043c029e5b69c6e
j-james <jj@j-james.me>
2019-08-06 14:30:29 -0700

    Write InsideFramePerimeter command (chute up?)

---
+++ src/main/java/com/spartronics4915/frc2019/commands/InsideFramePerimeter.java
@@ -0,0 +1,13 @@
+package com.spartronics4915.frc2019.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class InsideFramePerimeter extends CommandGroup
+{
+    public InsideFramePerimeter()
+    {
+        addParallel(new IntakeHold());
+        //  XXX: Does the chute need to be up to be in the frame perimeter?
+        addParallel(new ChuteUp());
+    }
+}
