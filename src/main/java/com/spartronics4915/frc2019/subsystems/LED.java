6519855d61cd64042335e1075d69caa7ac80990c
j-james <jj@j-james.me>
2019-08-05 19:37:01 -0700

    Rewrite LED.java (it's tiny)

--- src/main/java/com/spartronics4915/frc2019/subsystems/LED.java
+++ src/main/java/com/spartronics4915/frc2019/subsystems/LED.java
@@ -1,7 +1,6 @@
 package com.spartronics4915.frc2019.subsystems;
 
-import com.spartronics4915.lib.util.ILoop;
-import com.spartronics4915.lib.util.ILooper;
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class LED extends Subsystem
 {
@@ -17,122 +16,37 @@ public class LED extends Subsystem
         return mInstance;
     }
 
-    public enum WantedState
-    {
-        CLOSED, INTAKE,
-    }
-
-    private enum SystemState
-    {
-        CLOSING, INTAKING,
-    }
-
-    private WantedState mWantedState = WantedState.CLOSED;
-    private SystemState mSystemState = SystemState.CLOSING;
-
     private LED()
     {
         boolean success = true;
         try
         {
-            // Instantiate your hardware here
+            //  Instantiate your hardware here
         }
         catch (Exception e)
         {
             success = false;
-            logException("Couldn't instantiate hardware", e);
+            //  logException("Couldn't instantiate hardware", e);
         }
 
-        logInitialized(success);
-    }
-
-    private final ILoop mLoop = new ILoop()
-    {
-
-        @Override
-        public void onStart(double timestamp)
-        {
-            synchronized (LED.this)
-            {
-                mWantedState = WantedState.CLOSED;
-                mSystemState = SystemState.CLOSING;
-            }
-        }
-
-        @Override
-        public void onLoop(double timestamp)
-        {
-            synchronized (LED.this)
-            {
-                SystemState newState = defaultStateTransfer();
-                switch (mSystemState)
-                {
-                    case INTAKING:
-                        break;
-                    case CLOSING:
-                        stop();
-                        break;
-                    default:
-                        logError("Unhandled system state!");
-                }
-                mSystemState = newState;
-            }
-        }
-
-        @Override
-        public void onStop(double timestamp)
-        {
-            synchronized (LED.this)
-            {
-                stop();
-            }
-        }
-    };
-
-    private SystemState defaultStateTransfer()
-    {
-        SystemState newState = mSystemState;
-        switch (mWantedState)
-        {
-            case CLOSED:
-                newState = SystemState.CLOSING;
-                break;
-            case INTAKE:
-                newState = SystemState.INTAKING;
-                break;
-            default:
-                newState = SystemState.CLOSING;
-                break;
-        }
-        return newState;
-    }
-
-    public synchronized void setWantedState(WantedState wantedState)
-    {
-        mWantedState = wantedState;
+        //  logInitialized(success);
     }
 
     @Override
-    public void registerEnabledLoops(ILooper enabledLooper)
+    public void initDefaultCommand()
     {
-        enabledLooper.register(mLoop);
+        //  TODO: add default command (both retracted)
     }
 
-    @Override
+    //  @Override
     public boolean checkSystem(String variant)
     {
         return false;
     }
 
-    @Override
+    //  @Override
     public void outputTelemetry()
     {
 
-    }
-
-    @Override
-    public void stop()
-    {
-        // Stop your hardware here
     }
 }
