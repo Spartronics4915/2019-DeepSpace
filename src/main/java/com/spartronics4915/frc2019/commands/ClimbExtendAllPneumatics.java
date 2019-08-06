cfb07e79558dcbb9a081e643e9232aa8ea38704a
j-james <jj@j-james.me>
2019-08-06 13:40:35 -0700

    Properly write pneumatics

--- src/main/java/com/spartronics4915/frc2019/commands/ClimbExtendAllPneumatics.java
+++ src/main/java/com/spartronics4915/frc2019/commands/ClimbExtendAllPneumatics.java
@@ -1,14 +1,59 @@
-//  This isn't quite accurate. We had a delay built into the old code to account for weight imbalances.
-
 package com.spartronics4915.frc2019.commands;
 
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import com.spartronics4915.frc2019.subsystems.Climber;
 
-public class ClimbExtendAllPneumatics extends CommandGroup
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ClimbExtendAllPneumatics extends Command
 {
+    private Climber mClimber;
+
     public ClimbExtendAllPneumatics()
     {
-        addParallel(new ClimbExtendFrontPneumatics());
-        addParallel(new ClimbExtendBackPneumatics());
+        mClimber = Climber.getInstance();
+        //  Use requires() here to declare subsystem dependencies
+        requires(mClimber);
+    }
+
+    //  Called just before this Command runs the first time
+    @Override
+    protected void initialize()
+    {
+        setInterruptible(false);
+        setTimeout(1.5);
+
+        mClimber.extendFrontPneumatics();
+        mClimber.extendBackPneumatics();
+
+    }
+
+    //  Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute()
+    {
+        //  Activating pneumatics is instantenous
+    }
+
+    //  Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished()
+    {
+        if (isTimedOut())
+            return true;
+
+        return false;
+    }
+
+    //  Called once after isFinished returns true
+    @Override
+    protected void end()
+    {
+    }
+
+    //  Called when another command which requires one or more of the same
+    //  subsystems is scheduled to run
+    @Override
+    protected void interrupted()
+    {
     }
 }
