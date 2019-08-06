6e3b25f3eda85b367d9163c2299de45291bf36f3
j-james <jj@j-james.me>
2019-08-06 13:53:16 -0700

    Fixish ClimbIntake, isFinished() is broken

--- src/main/java/com/spartronics4915/frc2019/commands/ClimbIntake.java
+++ src/main/java/com/spartronics4915/frc2019/commands/ClimbIntake.java
@@ -1,38 +1,46 @@
-// ClimbIntake or IntakeClimb?
+//  ClimbIntake or IntakeClimb?
 //  Should it be descriptive of the subsystem or the command group?
 
 package com.spartronics4915.frc2019.commands;
 
-import com.spartronics4915.frc2019.Robot;
+import com.spartronics4915.frc2019.subsystems.CargoIntake;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class ClimbIntake extends Command
 {
+    private CargoIntake mCargoIntake;
+
     public ClimbIntake()
     {
+        mCargoIntake = CargoIntake.getInstance();
         //  Use requires() here to declare subsystem dependencies
-        requires(Robot.mCargoIntake);
+        requires(mCargoIntake);
     }
 
     //  Called just before this Command runs the first time
     @Override
     protected void initialize()
     {
+        //  Stop motors if they (somehow) are running
+        mCargoIntake.stop();
+
+        //  Putting the arm down should only be called once
+        mCargoIntake.armClimb();
     }
 
     //  Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute()
     {
-        Robot.mCargoIntake.armClimb();
+        mCargoIntake.intake();
     }
 
     //  Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished()
     {
-        //  XXX: What should be done here?
+        //  XXX: Should stop once the back pneumatic legs are up
         return false;
     }
 
@@ -40,6 +48,7 @@ public class ClimbIntake extends Command
     @Override
     protected void end()
     {
+        mCargoIntake.stop();
     }
 
     //  Called when another command which requires one or more of the same
