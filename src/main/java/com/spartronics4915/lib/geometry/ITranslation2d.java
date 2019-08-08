191b7987c2a6696a5444b80a9bb3bc5646156dc0
j-james <jj@j-james.me>
2019-08-07 19:46:15 -0700

    Remove lib

--- src/main/java/com/spartronics4915/lib/geometry/ITranslation2d.java
+++
@@ -1,7 +0,0 @@
-package com.spartronics4915.lib.geometry;
-
-public interface ITranslation2d<S> extends State<S>
-{
-
-    public Translation2d getTranslation();
-}
--- src/main/java/com/spartronics4915/lib/util/ILooper.java
+++
@@ -1,6 +0,0 @@
-package com.spartronics4915.lib.util;
-
-public interface ILooper
-{
-    void register(ILoop loop);
-}
