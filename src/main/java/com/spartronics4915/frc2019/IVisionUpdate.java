a61f46b36a5025409bffa59949bf4ff38047590c
j-james <jj@j-james.me>
2019-08-07 18:09:17 -0700

    Remove Autonomous and Paths

--- src/main/java/com/spartronics4915/frc2019/IVisionUpdate.java
+++
@@ -1,8 +0,0 @@
-package com.spartronics4915.frc2019;
-
-// Generic bounds must reference types in a parallel class
-// Arbitrary limitations are fun, I guess
-public interface IVisionUpdate
-{
-    boolean isEmpty();
-}
